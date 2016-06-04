860a5ca6c8d558e3d5e0fd99c561f550337dea7e
Michael Bagnall <mrbagnall@icloud.com>
2016-06-04 10:02:50 -0400

    Working through output issues

--- install.sh
+++ install.sh
@@ -80,10 +80,10 @@ tar -xzf $php
 
 echo "Installing OpenSSL 1.0.1t"
 cd openssl-1.0.1t
-./Configure --openssldir=/usr/local/ssl darwin64-x86_64-cc no-ssl2 no-ssl3 enable-sha >> ./logs/openssl-configure.txt
-make clean >> ./logs/openssl-make-clean.txt
-make >> ./logs/openssl-make.txt
-make install >> ./logs/openssl_install
+./Configure --openssldir=/usr/local/ssl darwin64-x86_64-cc no-ssl2 no-ssl3 enable-sha >> ../../logs/openssl-configure.txt 2>&1
+make clean >> ../../logs/openssl-make-clean.txt 2>&1
+make >> ../../logs/openssl-make.txt 2>&1
+make install >> ../../logs/openssl_install 2>&1
 
 exit 1;
 
